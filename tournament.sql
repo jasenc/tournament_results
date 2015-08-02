3c0b8eecac0da18020c38b32a7a7348778f84f43
jasenc <jasen.c8@gmail.com>
2015-08-02 07:23:53 -0700

    feat: complete playerStandings

--- tournament.sql
+++ tournament.sql
@@ -22,40 +22,16 @@ CREATE TABLE matches (id SERIAL primary key,
                       loser INTEGER REFERENCES players (id));
 
 CREATE VIEW player_standings AS
-    SELECT players.id,
-           players.name,
-           COUNT(matches.winner) AS wins,
-           COUNT(matches.winner + matches.loser) AS total_matches
-      FROM players
-      LEFT JOIN matches
-        ON players.id = matches.winner OR players.id = matches.loser
-     GROUP BY players.id
-     ORDER BY wins DESC;
-
-CREATE VIEW total_loses AS
-    SELECT players.id,
-           players.name,
-           COUNT(matches.loser) AS loses
-      FROM players
-      JOIN matches
-        ON players.id = matches.loser
-     GROUP BY players.id
-     ORDER BY loses;
---
--- CREATE VIEW total_matches AS
---    SELECT players.id,
---           players.name,
---           COUNT(matches.winner + matches.loser) AS total_matches
---      FROM players
---      JOIN matches
---        ON players.id = matches.winner AND players.id = matches.loser
---     GROUP BY players.id
---     ORDER BY players.id;
---
--- CREATE VIEW player_standings AS
---     SELECT players.id,
---            players.name,
---            total_wins.wins,
---            total_matches.total_matches
---       FROM players, total_wins, total_matches
---      WHERE players.id = total_wins.id AND total_wins.id = total_matches.id;
+     SELECT players.id,
+            players.name,
+            COUNT(matches1.winner) AS wins,
+            COUNT(matches2.winner + matches2.loser) AS total_matches
+       FROM players
+       -- Left join to ensure values with wins = 0 are not hidden
+       LEFT JOIN matches AS matches1
+         ON players.id = matches1.winner
+       -- Left join to ensure values with total_matches = 0 are not hidden
+       LEFT JOIN matches AS matches2
+         ON players.id = matches2.winner OR players.id = matches2.loser
+      GROUP BY players.id
+      ORDER BY wins DESC;
